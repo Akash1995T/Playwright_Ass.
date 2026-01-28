26d66fb29fb50660a651533a53743baea13be0fc
Akash1995T <tyagi1995akash@gmail.com>
2026-01-28 14:17:54 +0530

    Delete cookies.ts

--- cookies.ts
+++
@@ -1,8 +0,0 @@
-import { Page } from '@playwright/test';
-
-export async function acceptCookies(page: Page) {
-  const acceptButton = page.locator('button:has-text("Akzeptieren")');
-  if (await acceptButton.isVisible().catch(() => false)) {
-    await acceptButton.click();
-  }
-}
